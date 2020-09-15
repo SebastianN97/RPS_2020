f51a3233a84bd9770a24936163fc51f270979fef
SebastianN97 <sebbe132132@gmail.com>
2020-09-15 19:06:43 +0200

    Creating play button for rps. writing down test to see if button works

--- cypress/integration/userCanPlayRPS.feature.js
+++ cypress/integration/userCanPlayRPS.feature.js
@@ -3,5 +3,6 @@ describe('User can play RPS', () => {
     it('user can play against the computer', () => {
         cy.visit('http://localhost:3001')
         cy.contains('Rock Paper Scissor')
+        //cy.get('Play').click()
     })
 })
