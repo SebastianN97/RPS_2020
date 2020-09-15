72c1ea9a72d0561bd4729c5ecf9d2235852118fc
SebastianN97 <sebbe132132@gmail.com>
2020-09-15 20:21:19 +0200

    Creating rps function in src/rps.js. Function to see messages for what happens if computer wins, draws or loses. Not done yet, needs an end code for if method.

---
+++ src/rps.js
@@ -0,0 +1,40 @@
+
+function rpsFuction() {
+    
+
+
+}
+
+function check(computer, user) {
+    
+    //Computer has rock
+    if ( computer == 'rock' | user 'paper' ) {
+        result = 'win'
+    }
+    else if ( computer == 'rock' | user 'rock' ) {
+        result = 'draw'
+    }
+    else if ( computer == 'rock' | user 'scissor' ) {
+        result = 'lose'
+    }
+
+    //Computer has paper
+    else if ( computer == 'paper' | user 'scissor' ) {
+        result = 'win'
+    }
+    else if ( computer == 'paper' | user 'paper' ) {
+        result = 'draw'
+    }
+    else if ( computer == 'paper' | user 'rock') {
+        result = 'lose'
+    }
+
+    //Computer has scissor
+    else if ( computer == 'scissor' | user 'rock' ) {
+        result = 'win'
+    }
+    else if ( computer == 'scissor' | user 'scissor' ) {
+        result = 'draw'
+    }
+    else 
+}
