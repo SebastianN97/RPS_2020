bec102ddbb524fb0aa9dcdad0ea26a91d151dff3
SebastianN97 <sebbe132132@gmail.com>
2020-09-18 21:29:12 +0200

    Creating buttons for rps. Modifying rps.js

--- src/rps.js
+++ src/rps.js
@@ -1,38 +1,63 @@
 
+const rps = () => {
 
-function check(computer, user) {
-    
-    //Computer has rock
-    if ( computer == 'rock' | user 'paper' ) {
-        result = 'win'
-    }
-    else if ( computer == 'rock' | user 'rock' ) {
-        result = 'draw'
-    }
-    else if ( computer == 'rock' | user 'scissor' ) {
-        result = 'lose'
+    function computer() {
+        const random = ( Math.floor(Math.random() * 3) );
+        const choice = ( 'rock', 'paper', 'scissor' )
+        return choice = [random];
     }
 
-    //Computer has paper
-    else if ( computer == 'paper' | user 'scissor' ) {
-        result = 'win'
-    }
-    else if ( computer == 'paper' | user 'paper' ) {
-        result = 'draw'
-    }
-    else if ( computer == 'paper' | user 'rock') {
-        result = 'lose'
+    function random() {
+
+        if (random == 1) {
+            return 'rock'
+        }
+        else if (random == 2) {
+            return 'paper'
+        }
+        else (random == 3) {
+            return 'scissor'
+        }
     }
 
-    //Computer has scissor
-    else if ( computer == 'scissor' | user 'rock' ) {
-        result = 'win'
+
+
+    function check(computer, user) {
+        
+        //Computer has rock
+        if ( computer == 'rock' | user 'paper' ) {
+            result = 'You won!'
+        }
+        else if ( computer == 'rock' | user 'rock' ) {
+            result = 'It is a tie!'
+        }
+        else if ( computer == 'rock' | user 'scissor' ) {
+            result = 'You lost!'
+        }
+
+        //Computer has paper
+        else if ( computer == 'paper' | user 'scissor' ) {
+            result = 'You won!'
+        }
+        else if ( computer == 'paper' | user 'paper' ) {
+            result = 'It is a tie!'
+        }
+        else if ( computer == 'paper' | user 'rock') {
+            result = 'You lost!'
+        }
+
+        //Computer has scissor
+        else if ( computer == 'scissor' | user 'rock' ) {
+            result = 'You won!'
+        }
+        else if ( computer == 'scissor' | user 'scissor' ) {
+            result = 'It is a tie!'
+        }
+        else (
+            result = 'You lost!'
+        )
     }
-    else if ( computer == 'scissor' | user 'scissor' ) {
-        result = 'draw'
-    }
-    else (
-        result = 'lose'
-    )
+
+
+
 }
-
