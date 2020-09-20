ad2796d8ed80be8175c4eda29a8464c30d4dee74
SebastianN97 <sebbe132132@gmail.com>
2020-09-20 19:36:49 +0200

    Creating function for computers choice, mathrandom function. Creating compareHands function for player and computers choices.

--- src/rpsFunction.js
+++ src/rpsFunction.js
@@ -7,9 +7,43 @@ const rps = () => {
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
 
+        const computerOptions = ["rock", "paper", "scissor"]
+      
+        options.forEach( option => {
+            option.addEventListener("click", function() {
+            const computerNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOptions[computerNumber];
+            });
+        });
+    };
 
+    const compareHands = (playerChoice, computerChoice) => {
+        //Game texts
+        const winner = document.querySelector('.winner');
+        
+        // Getting a tie
+        if(playerChoice === computerChoice) {
+            winner.textContent = "You got tie!";
+            return;
+        }
+
+        // Player have rock
+        if(playerChoice === "rock") {
+            if(computerChoice === "scissor") {
+                winner.textContent = "You won!";
+                return;
+            }
+            else {
+                winner.textContent = "You lost!";
+                return;
+            };
+        };
+
+        // Player have paper
 
     };
+
+    playMatch();
     
 };
 
