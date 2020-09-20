205075ce1cdecdc02f97d17b4c1981df04cb8379
SebastianN97 <sebbe132132@gmail.com>
2020-09-20 19:01:55 +0200

    Starting with rps function. Creating const playMatch.

--- src/rpsFunction.js
+++ src/rpsFunction.js
@@ -1,3 +1,18 @@
 
+const rps = () => {
 
+
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+
+
+
+    };
+    
+};
+
+// start rps 
+rps();
 
